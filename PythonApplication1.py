423157549e33829a003b2177bd349ebb046e4381
hitcindy <670223558@qq.com>
2019-05-08 16:22:22 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -13,38 +13,6 @@ headers = {
     'Referer': 'https://m.weibo.cn/u/5680339910',
     'User-Agent': user_agent
 }
-
-# 按页数抓取数据
-def get_single_page(page):
-    params = {
-        'type': 'uid',
-        'value': 5680339910,#1665372775,
-        'containerid': 1076035680339910,
-        'page': page
-    }
-    url = base_url + urlencode(params)
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-    except requests.ConnectionError as e:
-        print('抓取错误', e.args)
-
-# 解析页面返回的json数据
-def parse_page(json):
-    items = json.get('data').get('cards')
-    for item in items:
-        item = item.get('mblog')
-        if item:
-            data = {
-                'id': item.get('id'),
-                'text': pq(item.get("text")).text(),  # 仅提取内容中的文本
-                'attitudes': item.get('attitudes_count'),
-                'comments': item.get('comments_count'),
-                'reposts': item.get('reposts_count')
-            }
-            yield data
-
 ##
 CK = {
     'type': 'uid',
@@ -206,20 +174,13 @@ if __name__ == '__main__':
     plt.xlabel('成员')
     # 设置纵轴标签
     plt.ylabel('粉丝个数')
-
     # 添加标题
     plt.title('G队成员微博粉丝数')
-
     # 添加纵横轴的刻度
     plt.xticks(index, xname,rotation=60)
-
     # 添加图例
     plt.legend(loc="upper right")
 
     plt.show()
-    #for page in range(1, 10):  # 抓取前十页的数据
-        #json = get_single_page(page)
-        #results = parse_page(json)
-        #for result in results:
-        #    print(result)
+
 
